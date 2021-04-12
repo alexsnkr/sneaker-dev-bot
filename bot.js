dd54035268a054ec9b29879deaf70f823b06ff9e
Alex <ChubbyBalboa@gmail.com>
2021-04-12 20:00:27 +0100

    Don't delete mod messages

--- bot.js
+++ bot.js
@@ -1,18 +1,20 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const channels = require('./channels')
-const { token } = require('./config.json')
+const { token, mod_roles } = require('./config.json')
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('message', (message) => {
-	const channelName = message.channel.name
+	if (message.channel.type === 'dm') return
 
-	if (channels[channelName]) {
-		channels[channelName](message)
-	}
+	const isMod = message.member.roles.cache.some((role) => mod_roles.includes(role.id))
+	if (isMod) return
+
+	const channelName = message.channel.name
+	channels[channelName]?.(message)
 })
 
 client.login(token)
